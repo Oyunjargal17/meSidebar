0ff528b08a2d2c573ab9aa14e77a465b73a2cb54
25LP1983 25LP1983 <25LP1983@admins-iMac.local>
2025-12-27 16:36:44 +0800

    12_27 301 angid

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const btn = document.querySelector(".btn");
 const menus = document.querySelector(".menus");
+const btnX = document.querySelector(".btnX");
 
 btn.addEventListener("click", () => {
-  menus.classList.toggle("menus");
+  menus.classList.toggle("active");
+  btn.classList.toggle("rotate");
+  btnX.classList.toggle("rotateX");
+});
+btnX.addEventListener("click", () => {
+  menus.classList.toggle("active");
 });
